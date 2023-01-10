b0f61c8c52dd4c830bd9ef0f15a29ebea18281f4
Guilherme <guigo_gaara@hotmail.com>
2023-01-09 22:49:14 -0300

    feat: add home page component

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class HomeComponent {
+
+}
