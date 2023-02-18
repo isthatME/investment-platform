f3ac94beeca5e902be2b795d32c3cddde52da65a
Guilherme <guigo_gaara@hotmail.com>
2023-02-18 00:18:35 -0300

    fix: change old import from app component test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,21 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import { AppShellComponent } from './app-shelll/app-shell/app-shell.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule, AppShellComponent],
     }).compileComponents();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(AppShellComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
