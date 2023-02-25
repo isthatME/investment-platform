59af5cfaaaecc7d3ef6880a064fd1f6a85c8c22b
Guilherme <guigo_gaara@hotmail.com>
2023-02-25 19:28:08 -0300

    refactor: remove duplicated folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppShellComponent } from './app-shelll/app-shell/app-shell.component';
+import { AppShellComponent } from './app-shell/app-shell.component';
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: 'home'},
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { AppShellComponent } from './app-shelll/app-shell/app-shell.component';
+import { AppShellComponent } from './app-shell/app-shell.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
