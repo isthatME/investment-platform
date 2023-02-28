a5475392ccfe2c2865454c857f76d041869f3d42
Guilherme <guigo_gaara@hotmail.com>
2023-02-27 22:14:06 -0300

    style: add banner layout for desktop

--- src/app/ui/components/banner/banner.component.ts
+++ src/app/ui/components/banner/banner.component.ts
@@ -8,7 +8,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   template: `
     <div class="banner-wrapper">
         <article>
-            <h1>Calculadora de investimentos</h1>
+            <h1>Calcule seus investimentos</h1>
             <h2>Calcule quanto os seus investimentos irão render. O cálculo é feito utilizando a formula de juros compostos.</h2>
         </article>
         <div class="image-wrapper">
