f2b7db79383be24ffc8819880b1e0223c76a473d
Guilherme <guigo_gaara@hotmail.com>
2023-03-04 20:59:35 -0300

    style: change header initial text

--- src/app/ui/components/banner/banner.component.ts
+++ src/app/ui/components/banner/banner.component.ts
@@ -9,7 +9,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
     <div class="banner-wrapper">
         <article>
             <h1>Calcule seus investimentos</h1>
-            <h2>Calcule quanto os seus investimentos irão render. O cálculo é feito utilizando a formula de juros compostos.</h2>
+            <h2>Maximize seu Potencial de Riqueza: Desbloqueie o Poder dos Juros Compostos com Nossa Ferramenta Avançada de Cálculo de Juros Compostos</h2>
         </article>
         <div class="image-wrapper">
         <img src="../../../assets/svg-icons/calculator.svg" alt="calculator-image"/>
