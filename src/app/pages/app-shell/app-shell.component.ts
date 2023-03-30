b9481429e49b2208eb8bc4f98a3c5fc8bab83606
Guilherme <guigo_gaara@hotmail.com>
2023-03-30 19:17:07 -0300

    refactor: move app-shell location

--- src/app/pages/app-shell/app-shell.component.ts
+++ src/app/pages/app-shell/app-shell.component.ts
@@ -1,7 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DarkModeToggleComponent } from 'src/app/ui/components/dark-mode-toggle/dark-mode-toggle.component';
-import { BannerComponent } from "../ui/components/banner/banner.component";
+import { BannerComponent } from "../../ui/components/banner/banner.component";
 @Component({
     selector: 'app-shell',
     standalone: true,
