0ce69487a53139ac07c26c44abc5c483de6640a9
Guilherme <guigo_gaara@hotmail.com>
2023-04-08 20:37:26 -0300

    chore: upgrade trypescript version

--- src/app/pages/interest-calculator/ui/compound-interest-form/compound-interest-form.component.ts
+++ src/app/pages/interest-calculator/ui/compound-interest-form/compound-interest-form.component.ts
@@ -1,12 +1,12 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
 
 @Component({
   selector: 'app-compound-interest-form',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, NgxMaskDirective],
+  imports: [CommonModule, ReactiveFormsModule, NgxMaskDirective, FormsModule],
   templateUrl: './compound-interest-form.component.html',
   styleUrls: ['./compound-interest-form.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
