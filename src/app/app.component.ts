7d983e2d7f9980b7ccaa80126505ece64bab665d
Guilherme <guigo_gaara@hotmail.com>
2023-04-29 20:09:41 -0300

    refactor: bootstrap aplication from app.module component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AppShellComponent } from './pages/app-shell/app-shell.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
 export const APP_COMPONENT_IMPORTS = [RouterModule, AppShellComponent];
 @Component({
   selector: 'app-root',
@@ -9,6 +11,12 @@ export const APP_COMPONENT_IMPORTS = [RouterModule, AppShellComponent];
       <router-outlet></router-outlet>
     </app-shell>
   `,
+  standalone: true,
+  imports: [
+    BrowserModule,
+    APP_COMPONENT_IMPORTS,
+    BrowserAnimationsModule
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
--- src/app/pages/interest-calculator/ui/compound-interest-form/compound-interest-form.component.ts
+++ src/app/pages/interest-calculator/ui/compound-interest-form/compound-interest-form.component.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
 
 @Component({
   selector: 'app-compound-interest-form',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, NgxMaskDirective, FormsModule],
+  imports: [CommonModule,FormsModule, ReactiveFormsModule, NgxMaskDirective],
   templateUrl: './compound-interest-form.component.html',
   styleUrls: ['./compound-interest-form.component.scss', '../../../../ui/components/button/button.scss', '../../../../ui/components/select/select.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -14,7 +14,7 @@ import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
 })
 export class CompoundInterestFormComponent {
 
-  form = this.fb.group({
+  form: FormGroup = this.fb.group({
     initialValue: [null, Validators.required],
     monthlyValue: [null, Validators.required],
     interestRate: [null, Validators.required],
