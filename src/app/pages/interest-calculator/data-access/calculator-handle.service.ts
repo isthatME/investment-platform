7c1bc384515097e416b65bea9ce27066d1fa200d
Guilherme <guigo_gaara@hotmail.com>
2023-12-11 13:55:18 -0300

    feat: compound interest service WIP

--- src/app/pages/interest-calculator/data-access/calculator-handle.service.ts
+++ src/app/pages/interest-calculator/data-access/calculator-handle.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@angular/core';
+interface calculationResponse {
+  totalAmount: number;
+  interestAmount: number;
+  totalAmountWithInterest: number;
+}
 
 @Injectable({
   providedIn: 'root',
@@ -9,7 +14,27 @@ export class CalculatorHandleService {
     monthlyValue: number,
     interestRate: { value: number; type: string },
     period: { value: number; type: string }
-  ): number {
-    return (initialValue * (Math.pow((1 + interestRate.value / 100), period.value))) - initialValue;
+  ): calculationResponse {
+    let totalAmount = initialValue;
+    let interestAmount = 0;
+    const interestRateValue =
+      interestRate.type === 'month'
+        ? interestRate.value / 100
+        : (interestRate.value / 12) / 100;
+    const periodValue =
+      period.type === 'month' ? period.value : period.value * 12;
+
+
+    for (let i = 0; i < periodValue; i++) {
+      interestAmount = initialValue * Math.pow(1 + interestRateValue, periodValue) - initialValue;
+      totalAmount += monthlyValue + (i === 0 ? 0 : interestAmount);
+      console.log(interestAmount, totalAmount)
+    }
+
+    return {
+      totalAmount: totalAmount,
+      interestAmount: interestAmount,
+      totalAmountWithInterest: totalAmount + interestAmount,
+    };
   }
 }
--- src/app/pages/interest-calculator/ui/base-interest-form/base-interest-form.component.ts
+++ src/app/pages/interest-calculator/ui/base-interest-form/base-interest-form.component.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
+import { CalculatorHandleService } from '../../data-access/calculator-handle.service';
 
 @Component({
   selector: 'app-base-interest-form',
@@ -15,25 +16,25 @@ import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
 export class BaseInterestFormComponent {
 
   form: FormGroup = this.fb.group({
-    initialValue: [null, Validators.required],
-    monthlyValue: [null, Validators.required],
+    initialValue: [1000, Validators.required],
+    monthlyValue: [100, Validators.required],
     interestRate: this.fb.group({
-      value: [null, Validators.required],
+      value: [1, Validators.required],
       type: ['month', Validators.required]
     }),
     period:  this.fb.group({
-      value: [null, Validators.required],
+      value: [1, Validators.required],
       type: ['month', Validators.required]
     }),
     calculatorType: ['compound-interest', Validators.required],
   });
 
   constructor(private readonly fb: FormBuilder) {}
-  // private readonly calculatorHandle = inject(CalculatorHandleService);
+  private readonly calculatorHandle = inject(CalculatorHandleService);
   onSubmit(): void{
-    // const value = this.calculatorHandle.compoundInterestHandle(this.form.value.initialValue, this.form.value.monthlyValue, this.form.value.interestRate, this.form.value.period)
+    this.calculatorHandle.compoundInterestHandle(this.form.value.initialValue, this.form.value.monthlyValue, this.form.value.interestRate, this.form.value.period)
   }
-  changeCalculatorType(event: any): void {
-    this.form.patchValue(event.target.value, {onlySelf: true})
+  formFieldChangeHandler(event: any): void {
+    this.form.patchValue({[event.target.name]: event.target.value}, {onlySelf: true})
   }
 }
